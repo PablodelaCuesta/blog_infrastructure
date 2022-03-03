5662c6b2956ce4ddde15f26be965e678af5f6d12
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2022-03-03 11:42:17 +0100

    change provider to aws and deleted resources

--- 00_prodiver.tf
+++ 00_prodiver.tf
@@ -1,24 +1,15 @@
 
+
+
 terraform {
   required_providers {
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "2.17.1"
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.3.0"
     }
   }
 }
 
-variable "digital_ocean_token" {}
-
-provider "digitalocean" {
+provider "aws" {
   # Configuration options
-  token = var.digital_ocean_token
-}
-
-
-## SSH Connection to remote machines
-
-resource "digitalocean_ssh_key" "default" {
-  name       = "Key from terraform"
-  public_key = file("~/.ssh/do/blog.pub")
 }
