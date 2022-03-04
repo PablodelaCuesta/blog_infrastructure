75e0e696f559f57d6f7607977243e2fa31b2d6c9
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2022-03-04 13:37:29 +0100

    added cname to domain config

--- 04_domain.tf
+++ 04_domain.tf
@@ -1,4 +1,11 @@
 resource "digitalocean_domain" "blog_domain" {
-  name = "pablodelacuesta.es"
-  ip_address = digitalocean_droplet.blog-public.ipv4_address
+  name       = "pablodelacuesta.es"
+  ip_address = digitalocean_loadbalancer.public-lb.ip
+}
+
+resource "digitalocean_record" "CNAME-www" {
+  domain = digitalocean_domain.blog_domain.name
+  type   = "CNAME"
+  name   = "www"
+  value  = "@"
 }
