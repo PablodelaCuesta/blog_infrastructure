bd95fe1dea835506ead307b3785cdc070f3a4dc1
Pablo de la Cuesta <pablodelacuesta@hotmail.es>
2022-04-22 07:51:34 +0200

    changes

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 
 # Kubernetes definition
 resource "digitalocean_kubernetes_cluster" "kubernetes_cluster" {
-  name = "k8s-production"
-  region = var.region
-  version = "1.22.8-do-0"
-  tags = [ "production" ]
-  vpc_uuid = digitalocean_vpc.vpc_blog
+  name     = "k8s-production"
+  region   = var.region
+  version  = "1.22.8-do-0"
+  tags     = ["production"]
+  vpc_uuid = digitalocean_vpc.vpc_blog.id
 
   node_pool {
-    name = "default-pool"
-    size = "s-1vcpu-2gb"
+    name       = "default-pool"
+    size       = "s-1vcpu-2gb"
     auto_scale = false
     node_count = 2
-    tags = [ "node-pool" ]
+    tags       = ["node-pool"]
   }
 }
 
